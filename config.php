825d8534f5b781d51c7220f6867d180f83ead977
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-11-27 15:20:45 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$host = '146.190.85.108';        // Database host
+$host = '127.0.0.1';        // Database host
 $db_name = 'supply_ease_new'; // Database name
 $username = 'supply_ease_user'; // Database username
 $password = 'supply_ease_password'; // Database password
