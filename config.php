c719b7019d59e8e4ce235dd4b8847cb466973974
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-11-27 15:21:41 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 $host = '127.0.0.1';        // Database host
-$db_name = 'supply_ease_new'; // Database name
-$username = 'supply_ease_user'; // Database username
-$password = 'supply_ease_password'; // Database password
+$db_name = 'supplyease_new'; // Database name
+$username = 'supplyease_user'; // Database username
+$password = 'supplyease_password'; // Database password
 
 // Create a connection
 $conn = new mysqli($host, $username, $password, $db_name);
