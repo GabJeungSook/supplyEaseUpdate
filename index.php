8c98f3c1c1a7ffc297655a7b8412525936349756
Juliet0720 <74673422+Juliet0720@users.noreply.github.com>
2024-12-01 17:42:56 +0800

    update

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ $products = mysqli_fetch_all($productsResult, MYSQLI_ASSOC);
     <!-- Products Section -->
     <div class="bg-white">
   <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <h2 class="font-semibold mb-4 text-xl">Products</h2>
+    <h2 class="font-semibold mb-4 text-2xl ">Products</h2>
     <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
     
     
